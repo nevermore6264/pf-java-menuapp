34c1e5e9090509390c33791e4e023cc2ee559c94
nevermore6264 <37317921+nevermore6264@users.noreply.github.com>
2018-03-18 23:38:39 +0700

    [Thực hành] Thiết kế menu cho ứng dụng Assignment

---
+++ src/hieutt/demo/MenuApplication.java
@@ -0,0 +1,58 @@
+package hieutt.demo;
+
+import java.util.Scanner;
+
+public class MenuApplication {
+    public static void main(String[] args) {
+        int choice;
+        Scanner input = new Scanner(System.in);
+        menu();
+        while (true) {
+            choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Draw the triangle");
+                    System.out.println("******");
+                    System.out.println("*****");
+                    System.out.println("****");
+                    System.out.println("***");
+                    System.out.println("**");
+                    System.out.println("*");
+                    menu();
+                    break;
+
+                case 2:
+                    System.out.println("Draw the square");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    menu();
+                    break;
+                case 3:
+                    System.out.println("Draw the rectangle");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    menu();
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("No choice!");
+            }
+        }
+
+    }
+
+    public static void menu() {
+        System.out.println("Menu");
+        System.out.println("1. Draw the triangle");
+        System.out.println("2. Draw the square");
+        System.out.println("3. Draw the rectangle");
+        System.out.println("0. Exit");
+        System.out.println("Enter your choice: ");
+    }
+}
